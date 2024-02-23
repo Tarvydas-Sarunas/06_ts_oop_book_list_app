9d33d58b9453a7d9f9999ecab9e2691f9c2c62a7
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-23 07:49:27 +0100

    add book from form

--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,17 @@ UI.showBooks();
 const b1 = new Book('Aplink pasauli', 'Z. Vernas', 542874);
 UI.addBook(b1);
 console.log('b1 ===', b1);
+const formEl = document.getElementById('book-form');
+formEl === null || formEl === void 0 ? void 0 : formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const authorEl = document.getElementById('author');
+    const isbnEl = document.getElementById('isbn');
+    const priceEl = document.getElementById('price');
+    if (!titleEl || !authorEl || !isbnEl || !priceEl)
+        return console.warn('toks elementas nerastas');
+    const newBook = new Book(titleEl.value, authorEl.value, +isbnEl.value);
+    console.log('newBook ===', newBook);
+    UI.addBook(newBook);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Book from './class/book.class.js';
 import UI from './class/ui.class.js';
+// import { BookIntereface } from './interface/inteface.js';
 
 console.log('Hello from ts');
 
@@ -20,3 +21,25 @@ const b1 = new Book('Aplink pasauli', 'Z. Vernas', 542874);
 // UI.createAndAppendOneRow(b1);
 UI.addBook(b1);
 console.log('b1 ===', b1);
+
+// nusitaikom i forma
+const formEl = document.getElementById('book-form') as HTMLTableSectionElement | null;
+// dedam event listineri
+formEl?.addEventListener('submit', (event: SubmitEvent): void => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement | null;
+  const authorEl = document.getElementById('author') as HTMLInputElement | null;
+  const isbnEl = document.getElementById('isbn') as HTMLInputElement | null;
+  const priceEl = document.getElementById('price') as HTMLInputElement | null;
+
+  // paimame reiksmes
+  if (!titleEl || !authorEl || !isbnEl || !priceEl) return console.warn('toks elementas nerastas');
+  const newBook = new Book(titleEl.value, authorEl.value, +isbnEl.value);
+  console.log('newBook ===', newBook);
+
+  // Todo.validation
+
+  // sukuriame nauja knyga
+  UI.addBook(newBook);
+  // issivalome inputus
+});
